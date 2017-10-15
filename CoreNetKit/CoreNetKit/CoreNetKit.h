c8244a8fc86087a75088914df4afbd8f27362521
Александр Кравченков <sprintend@gmail.com>
2017-10-15 07:54:56 +0300

    Kit components moved to new project and old project was removed

---
+++ CoreNetKit/CoreNetKit/CoreNetKit.h
@@ -0,0 +1,19 @@
+//
+//  CoreNetKit.h
+//  CoreNetKit
+//
+//  Created by Александр Кравченков on 15.10.2017.
+//  Copyright © 2017 Кравченков Александр. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreNetKit.
+FOUNDATION_EXPORT double CoreNetKitVersionNumber;
+
+//! Project version string for CoreNetKit.
+FOUNDATION_EXPORT const unsigned char CoreNetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreNetKit/PublicHeader.h>
+
+
