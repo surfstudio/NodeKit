d1bd9e1b1622c568658a5fc07c0a372978a74234
Alexander Kravchenkov <akravchenkov@surfstudio.co>
2018-04-04 07:10:45 +0300

    Now all contexts may have many listeners

---
+++ CoreNetKit/Kit/Support/Event.swift
@@ -0,0 +1,9 @@
+//
+//  Event.swift
+//  CoreNetKit
+//
+//  Created by Alexander Kravchenkov on 04.04.2018.
+//  Copyright © 2018 Кравченков Александр. All rights reserved.
+//
+
+import Foundation
