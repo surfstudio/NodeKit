bedc57ee21a4a55b402856845fde39ddc4915258
Alexander Kravchenkov <akravchenkov@surfstudio.co>
2018-04-05 06:43:09 +0300

    Added auth request to example

---
+++ Example/Example/Services/AuthRequest.swift
@@ -0,0 +1,9 @@
+//
+//  AuthRequest.swift
+//  Example
+//
+//  Created by Alexander Kravchenkov on 04.04.2018.
+//  Copyright © 2018 Кравченков Александр. All rights reserved.
+//
+
+import Foundation
---
+++ Example/Example/Services/URLs.swift
@@ -0,0 +1,9 @@
+//
+//  URLs.swift
+//  Example
+//
+//  Created by Alexander Kravchenkov on 04.04.2018.
+//  Copyright © 2018 Кравченков Александр. All rights reserved.
+//
+
+import Foundation
