ca57d64d5acf4cd03805b4051446476dd30d314d
Aleksandr Smirnov <avsmirnov567@gmail.com>
2018-05-21 16:10:19 +0500

    #25 Add public init to MultipartData

--- CoreNetKit/Core/ServerPart/MultipartData.swift
+++ CoreNetKit/Core/ServerPart/MultipartData.swift
@@ -13,4 +13,11 @@ public struct MultipartData {
     public let name: String
     public let fileName: String
     public let mimeType: String
+
+    public init(data: Data, name: String, fileName: String, mimeType: String) {
+        self.data = data
+        self.name = name
+        self.fileName = fileName
+        self.mimeType = mimeType
+    }
 }
