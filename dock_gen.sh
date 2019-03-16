8141860347495bf558736d0e5fe5a47ad3d272a7
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-16 14:02:09 +0300

    Fix travis

---
+++ dock_gen.sh
@@ -0,0 +1,8 @@
+jazzy \
+  --clean \
+  --author Alexander Kravchenkov \
+  --author_url https://lastsprint.dev \
+  --github_url https://github.com/surfstudio/CoreNetKit \
+  --xcodebuild-arguments -scheme,CoreNetKit \
+  --module CoreNetKit \
+  --output docs/swift_output \
