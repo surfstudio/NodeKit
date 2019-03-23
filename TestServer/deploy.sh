c3e501296b021e96ed958edcb62a01b7b38b2b60
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-24 02:29:37 +0300

    fix ci

--- TestServer/deploy.sh
+++ TestServer/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl 127.0.0.1:8811/shutdown
+curl 127.0.0.1:8844/shutdown
 cd /home/guest/stage/CoreNetKit/TestServer/src/TestServer
 go get github.com/gorilla/mux
 setsid go run TestServer.go
