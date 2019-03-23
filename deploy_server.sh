367cf34edde437b7c002702c1fa4f17e00ab51be
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-24 00:26:16 +0300

    fix ci

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,4 +1,5 @@
 scp -i ssh/id_rsa -P 22334 -r TestServer guest@lastsprint.dev:/home/guest/stage/CoreNetKit
+curl lastsprint.dev:8844/shutdown
 ssh -i ssh/id_rsa \
 -p 22334 \
 guest@lastsprint.dev \
