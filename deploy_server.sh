8d7247cb0bdd950015a1f95e498f1f03a08e31f9
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-23 22:52:26 +0300

    fix ci

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,8 +1,11 @@
 scp -i ssh/id_rsa -P 22334 -r TestServer guest@lastsprint.dev:/home/guest/stage/CoreNetKit
 ssh -i ssh/id_rsa \
 -p 22334 \
-guest@lastsprint.dev \
+guest@lastsprint.dev
+
 cd stage/CoreNetKit/TestServer/src/TestServer &&\
 go install &&\
 cd ../../bin &&\
 setsid TestServer
+
+exit
