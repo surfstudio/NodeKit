dc06cab3a8815e9cc48589f8472452c244adfbbf
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-24 13:26:38 +0300

    fix ci

---
+++ CoreNetKitIntegrationTests/UnitTests/CacheNode/FirstCachePolicyTests.swift
@@ -0,0 +1,9 @@
+//
+//  FirstCachePolicyTests.swift
+//  CoreNetKitIntegrationTests
+//
+//  Created by Александр Кравченков on 24/03/2019.
+//  Copyright © 2019 Кравченков Александр. All rights reserved.
+//
+
+import Foundation
