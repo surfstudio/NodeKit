2f05a3af7f2e3a0cb73e8853b8b03c80fe2cd747
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-24 15:44:12 +0300

    fix ci

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,9 +1,7 @@
-#scp -i ssh/id_rsa -P 22334 -r TestServer guest@lastsprint.dev:/home/guest/stage/CoreNetKit
-#ssh -i ssh/id_rsa \
-#-p 22334 \
-#guest@lastsprint.dev \
-#. stage/CoreNetKit/TestServer/deploy.sh
-
-TestServer/bin/TestServer 
+scp -i ssh/id_rsa -P 22334 -r TestServer guest@lastsprint.dev:/home/guest/stage/CoreNetKit
+ssh -i ssh/id_rsa \
+-p 22334 \
+guest@lastsprint.dev \
+. stage/CoreNetKit/TestServer/deploy.sh
 
 exit
