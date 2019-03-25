e8b015a9e29d13d56da2fd7c16d1f55da3fc4fd0
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-03-26 00:22:47 +0300

    Добавлен пример

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,19 +1,93 @@
 import UIKit
 import CoreNetKit
 
-enum Endpoints: UrlRouteProvider {
-    case data
+enum CustomError: Error {
+    case badUrl
+}
 
+enum Endpoint {
+    case isHttp2
+}
+
+extension URL {
+    static func from(_ string: String) throws -> URL {
+        guard let url = URL(string: string) else {
+            throw CustomError.badUrl
+        }
+        return url
+    }
+}
+
+extension Endpoint: UrlRouteProvider {
     func url() throws -> URL {
         switch self {
-        case .data:
-            return try "example.com/data".asURL()
+        case .isHttp2:
+            return try .from("https://http2.pro/api/v1")
         }
     }
 }
 
-func ret() -> Observer<Json> {
+/// Пример запроса без параметров.
+
+/// ---------------- Модели для запроса
+
+/// DTOConvertible (верхний слой)
+struct Http2CheckResult {
+    let status: Http2Status
+    let `protocol`: String
+    let push: Http2Status
+    let userAgent: String
+}
+
+extension Http2CheckResult: DTOConvertible {
+
+    typealias DTO = Http2CheckResultEntry
+
+    static func toModel(from model: Http2CheckResultEntry) throws -> Http2CheckResult {
+        return .init(status: model.http2,
+                     protocol: model.protocol,
+                     push: model.push,
+                     userAgent: model.user_agent)
+    }
+
+    func toDTO() throws -> Http2CheckResultEntry {
+        return .init(http2: self.status,
+                     protocol: self.protocol,
+                     push: self.push,
+                     user_agent: self.userAgent)
+    }
+}
+
+enum Http2Status: Int, Codable {
+    case used = 2
+    case notUsed = 1
+}
+
+/// RawMappable (нижний слой)
+
+struct Http2CheckResultEntry: Codable {
+    let http2: Http2Status
+    let `protocol`: String
+    let push: Http2Status
+    let user_agent: String
+}
+
+extension Http2CheckResultEntry: RawMappable {
+    typealias Raw = Json
+}
+
+func checkHttp2() -> Observer<Http2CheckResult> {
     return UrlChainsBuilder()
-        .default(with: UrlChainConfigModel(method: .get, route: Endpoints.data))
-        .process()
+            .default(with: UrlChainConfigModel(method: .get, route: Endpoint.isHttp2))
+            .process()
+}
+
+print("checkHttp2")
+
+checkHttp2().onCompleted { result in
+    print(result)
+}.onError { error in
+    print(error)
+}.defer {
+    print("defer")
 }
