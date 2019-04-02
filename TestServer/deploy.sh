6c1a2cb1794552c6b2cac5918ed9d2d9edafbf1d
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-04-02 10:05:42 +0300

    fix ci

--- TestServer/deploy.sh
+++ TestServer/deploy.sh
@@ -3,6 +3,6 @@ cd /home/guest/stage/CoreNetKit/TestServer/src/TestServer
 echo 'CD COMPLETE'
 go get github.com/gorilla/mux
 echo 'GO GET COMPLETE'
-nohup go run TestServer.go &
+go run TestServer.go
 echo 'RAN'
 echo '\n'
--- deploy_server.sh
+++ deploy_server.sh
@@ -2,4 +2,4 @@ scp -i ssh/id_rsa -P 22334 -r TestServer guest@lastsprint.dev:/home/guest/stage/
 ssh -i ssh/id_rsa \
 -p 22334 \
 guest@lastsprint.dev \
-stage/CoreNetKit/TestServer/deploy.sh
+stage/CoreNetKit/TestServer/deploy.sh &
