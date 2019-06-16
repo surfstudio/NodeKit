7bca00a02858628dbdd639145fd71def80066a2c
Alexander Kravchenkov <a.a.kravchenkov@gmail.com>
2019-06-16 12:23:11 +0300

    fix ci #6

--- ci/prepare_srv_deploy.sh
+++ ci/prepare_srv_deploy.sh
@@ -1,4 +1,4 @@
 eval "$(ssh-agent -s)"
 chmod 600 ssh/id_rsa
-echo -e "Host lastsprint.dev\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo "Host lastsprint.dev\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 ssh-add ssh/id_rsa
