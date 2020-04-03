9cad00000ad317cf580911ccbb1876684af10b15
Krupenko Validislav <dev.fixique@gmail.com>
2020-04-03 18:40:38 +0300

    #42 configure test for post request

---
+++ IntegrationTests/Infrastructure/Chains/CustomServiceChain.swift
@@ -0,0 +1,25 @@
+//
+//  CustomServiceChain.swift
+//  IntegrationTests
+//
+//  Created by Vladislav Krupenko on 03.04.2020.
+//  Copyright © 2020 Кравченков Александр. All rights reserved.
+//
+
+import Foundation
+
+@testable
+import NodeKit
+import Alamofire
+
+final class CustomServiceChain: UrlServiceChainBuilder {
+
+    override func urlResponseBsonProcessingLayerChain() -> Node<DataResponse<Data>, Bson> {
+        let responseDataParserNode = ResponseBsonDataParserNode()
+        let responseDataPreprocessorNode = ResponseBsonDataPreprocessorNode(next: responseDataParserNode)
+        let responseHttpErrorProcessorNode = ResponseHttpErrorProcessorNode(next: responseDataPreprocessorNode)
+        let responseCustomServerErrorProcessorNode = CustomServerErrorProcessorNode(next: responseHttpErrorProcessorNode)
+        return ResponseProcessorNode(next: responseCustomServerErrorProcessorNode)
+    }
+
+}
