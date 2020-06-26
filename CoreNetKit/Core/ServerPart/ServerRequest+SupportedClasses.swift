6a1911dad605deeef3d68593a24ea6d8561c5129
Krupenko Validislav <dev.fixique@gmail.com>
2020-06-26 13:19:03 +0300

    Add ability to add server trusty police

--- CoreNetKit/Core/ServerPart/ServerRequest+SupportedClasses.swift
+++ CoreNetKit/Core/ServerPart/ServerRequest+SupportedClasses.swift
@@ -21,14 +21,22 @@ public class ServerRequestsManager {
 
     let manager: SessionManager
 
+    private static var serverTrustPolicies: [String: ServerTrustPolicy] = [:]
+
     init() {
         let configuration = URLSessionConfiguration.default
         configuration.timeoutIntervalForResource = 60 * 3
         configuration.timeoutIntervalForRequest = 60 * 3
         configuration.requestCachePolicy = .reloadIgnoringCacheData
         configuration.urlCache = nil
-        self.manager = Alamofire.SessionManager(configuration: configuration)
+        self.manager = Alamofire.SessionManager(configuration: configuration,
+                                                serverTrustPolicyManager: ServerTrustPolicyManager(policies: ServerRequestsManager.serverTrustPolicies))
     }
+
+    class func setup(with serverTrustPolicies: [String: ServerTrustPolicy]) {
+        self.serverTrustPolicies = serverTrustPolicies
+    }
+
 }
 
 public enum CachePolicy {
