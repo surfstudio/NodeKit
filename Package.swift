0cb6b72cf9551bf9db17917e8a6f6bef77a5e8db
Nikita Somenkov <somenkov.nikita@icloud.com>
2020-09-26 03:46:38 +0300

    Add basic Swift Packet Manager support

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "NodeKit",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "NodeKit",
+            targets: ["NodeKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.0.0-beta.6")),
+        .package(url: "https://github.com/surfstudio/CoreEvents", .branch("add-swiftpm-support")),
+    ],
+    targets: [
+        .target(
+            name: "NodeKit",
+            dependencies: [
+                "Alamofire",
+                "CoreEvents",
+            ],
+            path: "NodeKit",
+            exclude: [
+                "Info.plist",
+            ]
+        ),
+        .testTarget(
+            name: "IntegrationTests",
+            dependencies: [
+                "NodeKit",
+            ],
+            path: "IntegrationTests",
+            exclude: [
+                "Info.plist",
+                "Resources/LICENSE.txt",
+            ]
+        ),
+    ]
+)
